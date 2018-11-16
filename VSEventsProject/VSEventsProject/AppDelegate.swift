244dac74faceda927e1e87785105b74fb889805a
Virgilius Santos <virgilius_santos@yahoo.com.br>
2018-11-16 20:44:08 -0200

    setup project

---
+++ VSEventsProject/VSEventsProject/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  VSEventsProject
+//
+//  Created by Virgilius Santos on 16/11/18.
+//  Copyright © 2018 Virgilius Santos. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+
+        return true
+    }
+}
+
